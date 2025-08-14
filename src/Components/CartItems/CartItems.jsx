7ccfb6b2b77b30ce6345a9a79075a1570dc095b7
Predator <anarbajaan1@gmail.com>
2025-08-14 20:54:19 +0500

    created logic for calculating total price in cart

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -5,7 +5,7 @@ import { useContext } from 'react'
 import remove_icon from '../Assets/cart_cross_icon.png'
 
 const CartItems = () => {
-  const { all_product, cartItems, removeFromCart } = useContext(ShopContext)
+  const { all_product, cartItems, removeFromCart, getTotalCartAmount } = useContext(ShopContext)
   return (
     <div className='cartitems'>
       <div className="cartitems-format-main">
@@ -33,7 +33,37 @@ const CartItems = () => {
             </div>
           )
         }
+        return null
       })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>Cart Totals</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Shipping Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <h3>Total</h3>
+              <h3>${getTotalCartAmount()}</h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promo code, enter it here</p>
+          <div className="cartitems-promobox">
+            <input type="text" placeholder='Promo code' />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
