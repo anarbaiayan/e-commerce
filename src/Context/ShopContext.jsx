3f618269a9eda4f9e0619acc2ff7b94258293abd
Predator <anarbajaan1@gmail.com>
2025-08-15 20:10:02 +0500

    started writing styles for adaptive web

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -33,8 +33,18 @@ const ShopContextProvider = (props) => {
     }
     return totalAmount
   }
+
+  const getTotalCartItems = () => {
+    let totalItem = 0
+    for(const item in cartItems) {
+      if(cartItems[item] > 0){
+        totalItem += cartItems[item]
+      }
+    }
+    return totalItem
+  }
   
-  const contextValue = {all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount}
+  const contextValue = {getTotalCartItems, all_product, cartItems, addToCart, removeFromCart, getTotalCartAmount}
 
   return (
     <ShopContext.Provider value={contextValue}>
